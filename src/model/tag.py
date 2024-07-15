e0235416af6d14d5be71c7298b84e2bac7d27b9a
stephane <sbornhauser77@gmail.com>
2024-07-15 05:51:09 +0200

    tag model prep

---
+++ src/model/tag.py
@@ -0,0 +1,16 @@
+
+
+class Tag:
+    """
+
+    """
+    def __init__(self, tag_type: str, tutorial_url: str = "", tutorial_name: str = ""):
+        """
+
+
+        Args:
+            tag_type:
+            tutorial_url:
+            tutorial_name:
+        """
+        pass
